ac65c68a6197d957dd4ff6c49e30367f2edf1fe3
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-08-29 21:02:38 +0300

    refactored code / added search post option

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
+import PostFilter from './components/PostFilter';
 import PostForm from './components/PostForm';
 
 import PostList from './components/PostList';
 import './styles/App.css';
-import MyInput from './UI/input/MyInput';
-import MySelect from './UI/select/MySelect';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -13,8 +12,20 @@ function App() {
     { id: 3, title: 'ббб', body: 'JavaScript -  программирования' },
   ]);
 
-  const [sort, setSort] = useState('');
-  const [searchInput, setSearchInput] = useState('');
+  const [filter, setFilter] = useState({ sort: '', query: '' });
+  const sortedPosts = useMemo(() => {
+    console.log('вызвано');
+
+    if (filter.sort) {
+      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
+    } else {
+      return posts;
+    }
+  }, [posts, filter.sort]);
+
+  const soretedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query));
+  }, [filter.query, sortedPosts]);
 
   function createPost(newPost) {
     setPosts([...posts, newPost]);
@@ -24,37 +35,13 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id));
   }
 
-  function onChangeSort(sort) {
-    setSort(sort);
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
-  }
-
   return (
     <div className="App">
       <PostForm createPost={createPost} />
       <hr style={{ margin: '15px 0' }} />
-      <div>
-        <MyInput
-          placeholder="Поиск..."
-          value={searchInput}
-          onChange={e => setSearchInput(e.target.value)}
-        />
-        <MySelect
-          value={sort}
-          onChangeSort={onChangeSort}
-          defaultValue="Сортировать по"
-          options={[
-            { value: 'title', name: 'По названию' },
-            { value: 'body', name: 'По описанию' },
-          ]}
-        />
-      </div>
+      <PostFilter filter={filter} setFilter={setFilter} />
 
-      {posts.length !== 0 ? (
-        <PostList posts={posts} title="Посты про JS" deletePost={deletePost} />
-      ) : (
-        <h1 style={{ textAlign: 'center', color: 'lightblue' }}>Посты не найдены!</h1>
-      )}
+      <PostList posts={soretedAndSearchedPosts} title="Посты про JS" deletePost={deletePost} />
     </div>
   );
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Post from './Post';
 function PostList({ posts, title, deletePost }) {
+  if (!posts.length) {
+    return <h1 style={{ textAlign: 'center', color: 'lightblue' }}>Посты не найдены!</h1>;
+  }
+
   return (
     <div>
       <h1 style={{ textAlign: 'center', color: 'lightblue' }}>{title}</h1>
